7cedccd67eb56cbca618c667fd0cf05efdc268dc
Mobile Aviation Tools <rob@robenanita.nl>
2016-06-17 16:11:01 +0200

    route tests

--- app/src/main/java/com/mobileaviationtools/flightsimplannerpro/MainActivity.java
+++ app/src/main/java/com/mobileaviationtools/flightsimplannerpro/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +12,7 @@ import com.mobileaviationtools.flightsimplannerpro.Airspaces.Airspace;
 import com.mobileaviationtools.flightsimplannerpro.Airspaces.Airspaces;
 import com.mobileaviationtools.flightsimplannerpro.Database.AirspacesDB;
 import com.mobileaviationtools.flightsimplannerpro.Database.DBFilesHelper;
+import com.mobileaviationtools.flightsimplannerpro.Route.RouteTest;
 import com.mobileaviationtools.flightsimplannerpro.TileWorkers.TileProviderFormats;
 import com.mobileaviationtools.flightsimplannerpro.TileWorkers.WmsTileWorker;
 
@@ -31,7 +33,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Get the map view and add a map.
-        final MapView mapView = (MapView)this.findViewById(R.id.mapView1);
+        //final MapView mapView = (MapView)this.findViewById(R.id.mapView1);
+
+		final MapView mapView = new MapView(this){
+			@Override
+			public  boolean onTouchEvent(MotionEvent event)
+			{
+
+				return false;
+			}
+		};
+
 		mapView.addInternetMap("MapQuest Aerial",
 				"http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg",
 				"", 		//Subdomains
@@ -78,24 +90,12 @@ public class MainActivity extends AppCompatActivity {
         chartBundlemapInfo.setTileProvider(chartBundleFactory);
         //mapView.addMapUsingMapInfo(chartBundlemapInfo);
 
-
+		// Primary location Netherlands
 		Location3D loc = new Location3D();
 		loc.altitude = 600000;
 		loc.longitude = 5.6129;
 		loc.latitude = 52.302;
 		mapView.setLocation3D(loc, 1);
-		//loc = mapView.getLocation3D();
-		//mapView.setLocationThatFitsCoordinates();
-
-		Button locFinderButton = (Button) this.findViewById(R.id.locationTest);
-		locFinderButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Location3D loc;// = new Location3D();
-				loc = mapView.getLocation3D();
-				Log.i("test", "test");
-			}
-		});
 
 		DBFilesHelper.CopyDatabases(this.getApplicationContext());
 		AirspacesDB airspacesDB = new AirspacesDB(this);
@@ -119,6 +119,11 @@ public class MainActivity extends AppCompatActivity {
 		for (Airspace airspace: airspaces) {
 			mapView.addPolygonToVectorMap("Airspaces", airspace.getAirspaceB3aLocations(), polygonStyle);
 		}
+
+		RouteTest routeTest = new RouteTest(this);
+		routeTest.placeRouteOnMap(mapView);
+
+
 
     }
 }
---
+++ app/src/main/java/com/mobileaviationtools/flightsimplannerpro/Route/RouteTest.java
@@ -0,0 +1,103 @@
+package com.mobileaviationtools.flightsimplannerpro.Route;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.PointF;
+
+import com.mobileaviationtools.flightsimplannerpro.R;
+
+import java.util.ArrayList;
+
+import us.ba3.me.Location;
+import us.ba3.me.MapView;
+import us.ba3.me.VectorMapInfo;
+import us.ba3.me.markers.DynamicMarker;
+import us.ba3.me.markers.DynamicMarkerMapInfo;
+import us.ba3.me.styles.LineStyle;
+
+/**
+ * Created by Rob Verhoef on 17-6-2016.
+ */
+public class RouteTest {
+    public RouteTest(Context context)
+    {
+        this.context = context;
+    }
+
+    private Context context;
+
+    public void placeRouteOnMap(MapView mapView)
+    {
+        VectorMapInfo vectorMapInfo = new VectorMapInfo();
+        vectorMapInfo.name = "route";
+        vectorMapInfo.zOrder = 10;
+        vectorMapInfo.alpha = 0.75f;
+        mapView.addMapUsingMapInfo(vectorMapInfo);
+
+        //Create a style for the route line
+        LineStyle lineStyle = new LineStyle();
+        lineStyle.outlineColor = Color.BLACK;
+        lineStyle.outlineWidth = 3;
+        lineStyle.strokeColor = Color.RED;
+        lineStyle.strokeWidth = 9;
+
+        //Get the route points
+        Location[] routePoints = createRoutePoints();
+
+        //Add the line to the vector layer
+        mapView.addLineToVectorMap("route", routePoints, lineStyle);
+
+        //Add markers
+        this.addMarkers(mapView, routePoints);
+
+    }
+
+    private void addMarkers(MapView mapView, Location[] routePoints)
+    {
+        DynamicMarkerMapInfo mapInfo = new DynamicMarkerMapInfo();
+        mapInfo.name = "Markers";
+        mapInfo.zOrder = 11;
+        mapView.addMapUsingMapInfo(mapInfo);
+
+        //Add a markers
+        DynamicMarker startPoint = new DynamicMarker();
+        startPoint.name = "startPoint";
+        startPoint.setImage(getBitmapFromDrawable(R.drawable.greendot), false);
+        startPoint.anchorPoint = new PointF(16,32);
+        startPoint.location = routePoints[0];
+        mapView.addDynamicMarkerToMap("Markers", startPoint);
+
+        DynamicMarker endPoint = new DynamicMarker();
+        endPoint.name = "endPoint";
+        endPoint.setImage(getBitmapFromDrawable(R.drawable.bluedot), false);
+        endPoint.anchorPoint = new PointF(16,32);
+        endPoint.location = routePoints[routePoints.length-1];
+        mapView.addDynamicMarkerToMap("Markers", endPoint);
+    }
+
+    private Bitmap getBitmapFromDrawable(Integer name)
+    {
+        return BitmapFactory.decodeResource(context.getResources(), name);
+    }
+
+    private Location[] createRoutePoints(){
+        ArrayList<Location> locations = new ArrayList<Location>();
+        locations.add(new Location(52.472409,4.320374));
+        locations.add(new Location(52.7399285, 7.182312));
+        //locations.add(new Location(35.77431930112859,-78.64204765649093));
+        //locations.add(new Location(35.77567608136691,-78.64196220184964));
+        //locations.add(new Location(35.77700395100487,-78.64187508617212));
+        //locations.add(new Location(35.77832583463119,-78.64183947388348));
+        //locations.add(new Location(35.77827906552807,-78.64020036607604));
+        //locations.add(new Location(35.77818133831267,-78.63823940725123));
+        //locations.add(new Location(35.7781182250745,-78.63659407693116));
+        //locations.add(new Location(35.77684002315468,-78.63668612449102));
+        //locations.add(new Location(35.77552738790911,-78.63672620342922));
+        Location[] locationArray = locations.toArray(new Location[locations.size()]);
+        return locationArray;
+    }
+
+
+}
