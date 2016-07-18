ebbb2c1c4e587345c2f40fb204eb6c16f30095bc
Mobile Aviation Tools <rob@robenanita.nl>
2016-07-18 17:36:54 +0200

    layout changes and PDF reader

---
+++ app/src/main/java/com/mobileaviationtools/flightsimplannerpro/MapOverlay/PDF.java
@@ -0,0 +1,18 @@
+package com.mobileaviationtools.flightsimplannerpro.MapOverlay;
+
+import android.graphics.pdf.PdfRenderer;
+
+/**
+ * Created by Rob Verhoef on 18-7-2016.
+ */
+public class PDF {
+    public PDF()
+    {
+
+    }
+
+    public void test()
+    {
+        //PdfRenderer
+    }
+}
